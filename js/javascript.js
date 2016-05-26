7fda138ed27abd9a5f602a7e5cffda0ee5e73f86
Carlos Fabian Colman <ingcarloscolman@gmail.com>
2016-05-26 19:39:45 -0400

    Archivos adicionales

---
+++ js/javascript.js
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>UniWEB</title>
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">
+</head>
+<body>
+
+<h1>ESTA ES UNA NUEVA WEB Ahora</h1>
+
+<nav>
+	<ul>
+		<li><a href="#" title="inicio de pagina">INICIO</a></li>
+		<li><a href="#" title="contactos de lista">CONTACTOS</a></li>
+		<li><a href="#" title="ventas">PRODUCTOS</a></li>
+		<li><a href="#" title="fotos">GALERIA</li></a>
+	</ul>
+</nav>
+
+<p>
+	<b>Lorem ipsum </b> dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+	<h2>Segundo Titulo</h2>
+	<h3>Tercer Titulo</h3>
+	<h4>Cuarto Titulo</h4>
+</p>
+
+<footer>
+	Power by ..>>Carlos Colman<<.. ;-)
+</footer>
+
+</body>
+</html>
